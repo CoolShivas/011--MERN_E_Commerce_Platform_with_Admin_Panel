a466a682e6f541c48fa76b15fa45de14178b30bf
CoolShivas <shivas2710cool00@gmail.com>
2025-09-25 12:01:59 +0530

    SettingUp the packageJSON, gitIgnore and formation of server port.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log(
-  "Starting of MERN ECommerce Platfrom with full backend and Admin Panel."
-);
+import express from "express";
+
+const server = express();
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running at Port :-) ${PORT}`);
+});
