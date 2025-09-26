9fa5aa0d73f981cf50efb70005903628abf1f7e2
CoolShivas <shivas2710cool00@gmail.com>
2025-09-26 15:33:59 +0530

    Deducting the quantity and price by decrement functionality in the Controllers/cart.js file having the addToCartProduct function.

--- Controllers/cart.js
+++ Controllers/cart.js
@@ -52,3 +52,65 @@ export const addToCartProduct = async (req, res) => {
 
 ///////***********************************************************************///////
 ///////***********************************************************************///////
+
+// // // Starting of Deduct Product to particular User's Cart function;
+
+export const deductToCartProductQty = async (req, res) => {
+  // // // Getting all the things from the request body of POSTMAN;
+  const { productId, quantity } = req.body;
+
+  // // // Giving the hard coded user id to add the items in specific user cart;
+  let userId = "68d50b0461b0791ae03d8e2c";
+
+  // // // Finding the user by it's id token whether it's cart exits or not;
+  let decreaseUserCart = await Cart.findOne({ userId });
+
+  if (!decreaseUserCart) {
+    // // // If user don't have a cart making the new cart to the specific user;
+    decreaseUserCart = new Cart({ userId, items: [] });
+  }
+
+  // // // Finding the index of the product if that product index already exists;
+  let itemIndex = decreaseUserCart.items.findIndex((proIndex) => {
+    return proIndex.productId.toString() === productId;
+  });
+  // // // After getting the index number of the product index that already exists. Then, increase the quantity of that product and also price too ;
+  if (itemIndex > -1) {
+    // // // Making new variable that contains the cart with all items of the specific user;
+    const havingProdItem = decreaseUserCart.items[itemIndex];
+
+    // // // If quantity greater than having cart quantity;
+    if (havingProdItem.quantity > quantity) {
+      // // // Reducing the quantity and price of the cart items;
+      const pricePerUnit = havingProdItem.price / havingProdItem.quantity;
+
+      havingProdItem.quantity -= quantity;
+      havingProdItem.price -= pricePerUnit * quantity;
+    } else {
+      // // // splice(startIndex, deleteCount) → removes (or replaces) elements from an array in place.
+      decreaseUserCart.items.splice(itemIndex, 1);
+    }
+  } else {
+    console.log("Invalid product id");
+    return res.json({ message: "Invalid product id", success: false });
+  }
+
+  // // // Saving the specific user's cart on the database;
+  await decreaseUserCart.save();
+
+  // // // Open the POSTMAN and select the POST request then enter the URL as (http://localhost:8000/api/cart/addcart) then fill the data on the body then hit the send btn. You will get the response both on POSTMAN and Terminal;
+  console.log(
+    "Item deducted from the user cart successfully..! => ",
+    decreaseUserCart
+  ); // // Getting decrease data and blank items array;
+  res.json({
+    message: "Item deducted from the user cart successfully..!",
+    success: true,
+    data: decreaseUserCart,
+  }); // // Getting decrease data and blank items array;
+};
+
+// // // Ending of Deduct Product to particular User's Cart function;
+
+///////***********************************************************************///////
+///////***********************************************************************///////
--- Routes/cart.js
+++ Routes/cart.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { addToCartProduct } from "../Controllers/cart.js";
+import {
+  addToCartProduct,
+  deductToCartProductQty,
+} from "../Controllers/cart.js";
 
 const router = express.Router();
 // // // @api description :- adding the products to specific user's cart;
 // // // @api method :- post
 // // // @api endPoint :- /api/product/addcart
 router.post("/addcart", addToCartProduct);
+// // // @api description :- decreasing the products of specific user's cart;
+// // // @api method :- delete
+// // // @api endPoint :- /api/product/dedcart
+router.delete("/dedcart", deductToCartProductQty);
 
 export default router;
