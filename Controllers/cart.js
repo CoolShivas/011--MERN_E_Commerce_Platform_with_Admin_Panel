bee18e6c6dbeb237cc59f241adaf3a3834b7c437
CoolShivas <shivas2710cool00@gmail.com>
2025-09-26 14:38:33 +0530

    Adding the quantity and price by increment functionality in the Controllers/cart.js file having the addToCartProduct function.

--- Controllers/cart.js
+++ Controllers/cart.js
@@ -12,11 +12,26 @@ export const addToCartProduct = async (req, res) => {
   // // // Giving the hard coded user id to add the items in specific user cart;
   let userId = "68d50b0461b0791ae03d8e2c";
 
-  // // // If user don't have a cart making the new cart to the specific user;
-  let createUserCart = new Cart({ userId, items: [] });
+  // // // Finding the user by it's id token whether it's cart exits or not;
+  let createUserCart = await Cart.findOne({ userId });
 
-  // // // Pushing the items to cart of a specific user's cart;
-  createUserCart.items.push({ productId, title, price, quantity, imgSrc });
+  if (!createUserCart) {
+    // // // If user don't have a cart making the new cart to the specific user;
+    createUserCart = new Cart({ userId, items: [] });
+  }
+
+  // // // Finding the index of the product if that product index already exists;
+  let itemIndex = createUserCart.items.findIndex((proIndex) => {
+    return proIndex.productId.toString() === productId;
+  });
+  // // // After getting the index number of the product index that already exists. Then, increase the quantity of that product and also price too ;
+  if (itemIndex > -1) {
+    createUserCart.items[itemIndex].quantity += quantity;
+    createUserCart.items[itemIndex].price += price * quantity;
+  } else {
+    // // // Pushing the items to cart of a specific user's cart;
+    createUserCart.items.push({ productId, title, price, quantity, imgSrc });
+  }
 
   // // // Saving the specific user's cart on the database;
   await createUserCart.save();
