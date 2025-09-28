98ff4f97dd4f9fc022f12e402cf61cb1304ddd39
CoolShivas <shivas2710cool00@gmail.com>
2025-09-28 16:10:38 +0530

    Adding the Ui on ShowProduct.jsx file and rendering the data through map method.

--- client/src/components/product/ShowProducts.jsx
+++ client/src/components/product/ShowProducts.jsx
@@ -6,13 +6,46 @@ const ShowProducts = () => {
   // console.log(pasApiProducts[0].title); // // Getting data on Browser's Console;
   return (
     <>
-      {pasApiProducts?.map((cur) => {
-        return (
-          <div key={cur._id}>
-            <h1>{cur.title}</h1>
-          </div>
-        );
-      })}
+      <div className="container d-flex justify-content-center align-items-center">
+        <div className="row container d-flex justify-content-center align-items-center my-5">
+          {pasApiProducts?.map((cur) => {
+            return (
+              <div
+                key={cur._id}
+                className="my-3 col-md-4 d-flex justify-content-center align-items-center"
+              >
+                <div
+                  className="card bg-dark text-light text-center"
+                  style={{ width: "18rem" }}
+                >
+                  <div className="d-flex justify-content-center align-items-center p-3">
+                    <img
+                      src={cur.imgSrc}
+                      alt="image not found"
+                      className="card-img-top"
+                      style={{
+                        width: "200px",
+                        height: "200px",
+                        borderRadius: "10px",
+                        border: "2px solid yellow",
+                      }}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">{cur.title}</h5>
+                    <div className="my-3">
+                      <button className="btn btn-primary mx-3">
+                        {cur.price} {"₹"}
+                      </button>
+                      <button className="btn btn-warning">Add To Cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
