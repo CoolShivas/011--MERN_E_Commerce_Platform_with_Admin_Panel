4b76b1eaa894938dbda95581c1e466bb73c5bbbb
CoolShivas <shivas2710cool00@gmail.com>
2025-09-28 14:30:35 +0530

    Installation of npm i axios. Now, integrating the Back-End data to Front-End.

--- client/src/context/AppState.jsx
+++ client/src/context/AppState.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppContext from "./AppContext";
+import axios from "axios";
 
 const AppState = (props) => {
   // // // Passing random data on client side for testing;
   const data = "Getting the data from Context API.";
+
+  // // // Use of Back-End API (http://localhost:8000/api/product/allproduct)
+  const URL = "http://localhost:8000/api";
+
+  useEffect(() => {
+    // // // Formation of fetch product function;
+    const fetchAllProducts = async () => {
+      const backendAPI = await axios.get(`${URL}/product/allproduct`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      console.log(backendAPI); // // Getting data on Browser's Console from Back-End API;
+    };
+    // // // Calling the function here;
+    fetchAllProducts();
+  }, []);
+
   return (
     <AppContext.Provider value={{ data }}>{props.children}</AppContext.Provider>
   );
