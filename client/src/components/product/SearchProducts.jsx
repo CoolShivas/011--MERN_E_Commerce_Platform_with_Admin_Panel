3237f1f88015a676744e991c9701ecf24cd6af57
CoolShivas <shivas2710cool00@gmail.com>
2025-09-29 13:16:54 +0530

    Use of useParams hook to grab the search term entered by user on search tag then goes to URL from url grabing that term.

--- client/src/components/product/SearchProducts.jsx
+++ client/src/components/product/SearchProducts.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const SearchProducts = () => {
+  // // // Using useParams hook to grab the dynamic id i.e.,("/product/search/:term") espically the term that is connected with the help of Navbar search tag using navigate(`/product/search/${searchTerm}`);
+  const { term } = useParams();
+
+  console.log(useParams());
+
   return (
     <>
       <center>
         <h1>Welcome to the Search Product Page</h1>
-        <h2>Getting search term on URL also.</h2>
+        <h2>Getting search term on URL as = {term}.</h2>
       </center>
     </>
   );
