59fc83e3c49820a211744334564c8a9a132d00d3
CoolShivas <shivas2710cool00@gmail.com>
2025-10-01 16:53:03 +0530

    Implementing the logic of productFilteration and priceFilteration on the Navbar.jsx using useContext and AppContext.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import AppContext from "../context/AppContext";
 
 const Navbar = () => {
+  const { pasApiProducts, setFIlteredProducts } = useContext(AppContext);
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+
+  const productFileration = (categories) => {
+    setFIlteredProducts(
+      pasApiProducts.filter((data) => {
+        return data.category.toLowerCase() === categories.toLowerCase();
+      })
+    );
+  };
+
+  const priceFileration = (pricing) => {
+    setFIlteredProducts(
+      pasApiProducts.filter((data) => {
+        return data.price >= pricing;
+      })
+    );
+  };
 
   const handlerOnSearchSubmit = (event) => {
     event.preventDefault();
@@ -48,16 +66,39 @@ const Navbar = () => {
           </div>
         </div>
         <div className="sub_bar">
-          <div className="items">No Filter</div>
-          <div className="items">Mobiles</div>
-          <div className="items">Laptops</div>
-          <div className="items">Cameras</div>
-          <div className="items">Headphones</div>
-          <div className="items">15999</div>
-          <div className="items">25999</div>
-          <div className="items">49999</div>
-          <div className="items">69999</div>
-          <div className="items">89999</div>
+          <div
+            className="items"
+            onClick={() => setFIlteredProducts(pasApiProducts)}
+          >
+            No Filter
+          </div>
+          <div className="items" onClick={() => productFileration("mobile")}>
+            Mobiles
+          </div>
+          <div className="items" onClick={() => productFileration("laptop")}>
+            Laptops
+          </div>
+          <div className="items" onClick={() => productFileration("camera")}>
+            Cameras
+          </div>
+          <div className="items" onClick={() => productFileration("headphone")}>
+            Headphones
+          </div>
+          <div className="items" onClick={() => priceFileration(15999)}>
+            15999
+          </div>
+          <div className="items" onClick={() => priceFileration(25999)}>
+            25999
+          </div>
+          <div className="items" onClick={() => priceFileration(49999)}>
+            49999
+          </div>
+          <div className="items" onClick={() => priceFileration(69999)}>
+            69999
+          </div>
+          <div className="items" onClick={() => priceFileration(89999)}>
+            89999
+          </div>
         </div>
       </div>
     </>
