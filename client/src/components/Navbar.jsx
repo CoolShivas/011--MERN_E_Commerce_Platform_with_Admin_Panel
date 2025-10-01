f68170e74cb211ec8559b1c25e35466312209fdc
CoolShivas <shivas2710cool00@gmail.com>
2025-10-01 16:59:00 +0530

    Use of useLocation hook and render the filter products and price navbar on Home page only.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import AppContext from "../context/AppContext";
 
 const Navbar = () => {
   const { pasApiProducts, setFIlteredProducts } = useContext(AppContext);
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
 
   const productFileration = (categories) => {
     setFIlteredProducts(
@@ -65,41 +66,46 @@ const Navbar = () => {
             <button className="btn btn-warning mx-2">logout</button>
           </div>
         </div>
-        <div className="sub_bar">
-          <div
-            className="items"
-            onClick={() => setFIlteredProducts(pasApiProducts)}
-          >
-            No Filter
+        {location.pathname === "/" && (
+          <div className="sub_bar">
+            <div
+              className="items"
+              onClick={() => setFIlteredProducts(pasApiProducts)}
+            >
+              No Filter
+            </div>
+            <div className="items" onClick={() => productFileration("mobile")}>
+              Mobiles
+            </div>
+            <div className="items" onClick={() => productFileration("laptop")}>
+              Laptops
+            </div>
+            <div className="items" onClick={() => productFileration("camera")}>
+              Cameras
+            </div>
+            <div
+              className="items"
+              onClick={() => productFileration("headphone")}
+            >
+              Headphones
+            </div>
+            <div className="items" onClick={() => priceFileration(15999)}>
+              15999
+            </div>
+            <div className="items" onClick={() => priceFileration(25999)}>
+              25999
+            </div>
+            <div className="items" onClick={() => priceFileration(49999)}>
+              49999
+            </div>
+            <div className="items" onClick={() => priceFileration(69999)}>
+              69999
+            </div>
+            <div className="items" onClick={() => priceFileration(89999)}>
+              89999
+            </div>
           </div>
-          <div className="items" onClick={() => productFileration("mobile")}>
-            Mobiles
-          </div>
-          <div className="items" onClick={() => productFileration("laptop")}>
-            Laptops
-          </div>
-          <div className="items" onClick={() => productFileration("camera")}>
-            Cameras
-          </div>
-          <div className="items" onClick={() => productFileration("headphone")}>
-            Headphones
-          </div>
-          <div className="items" onClick={() => priceFileration(15999)}>
-            15999
-          </div>
-          <div className="items" onClick={() => priceFileration(25999)}>
-            25999
-          </div>
-          <div className="items" onClick={() => priceFileration(49999)}>
-            49999
-          </div>
-          <div className="items" onClick={() => priceFileration(69999)}>
-            69999
-          </div>
-          <div className="items" onClick={() => priceFileration(89999)}>
-            89999
-          </div>
-        </div>
+        )}
       </div>
     </>
   );
