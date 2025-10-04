1d8402b8cd1a73ebf6478ed09f87fb8e9abccde4
CoolShivas <shivas2710cool00@gmail.com>
2025-10-04 18:33:18 +0530

    Completion of Ui of CheckOut  page.

--- client/src/components/CheckOut.jsx
+++ client/src/components/CheckOut.jsx
@@ -3,9 +3,47 @@ import React from "react";
 const CheckOut = () => {
   return (
     <>
-      <center>
-        <h1>Welcome to the Check-Out Page.</h1>
-      </center>
+      <div className="container  my-3">
+        <h1 className="text-center">Order Summary</h1>
+
+        <table className="table table-bordered border-primary bg-dark">
+          <thead className="bg-dark">
+            <tr>
+              <th scope="col" className="bg-dark text-light text-center">
+                Product Details
+              </th>
+
+              <th scope="col" className="bg-dark text-light text-center">
+                Shipping Address
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-dark">
+            <tr>
+              <td className="bg-dark text-light"></td>
+              <td className="bg-dark text-light">
+                <ul style={{ fontWeight: "bold" }}>
+                  <li>Name :</li>
+                  <li>Phone : </li>
+                  <li>Country : </li>
+                  <li>State : </li>
+                  <li>PinCode : </li>
+                  <li>Near By : </li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="container text-center my-5">
+        <button
+          className="btn btn-secondary btn-lg"
+          style={{ fontWeight: "bold" }}
+        >
+          Procced To Pay
+        </button>
+      </div>
     </>
   );
 };
