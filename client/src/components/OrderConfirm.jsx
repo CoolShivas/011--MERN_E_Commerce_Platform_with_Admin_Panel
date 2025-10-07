93e5f9e097404a1a1a52881eadc06b8983ee794d
CoolShivas <shivas2710cool00@gmail.com>
2025-10-07 17:26:03 +0530

    Completion of Ui of OrderConfirm page.

--- client/src/components/OrderConfirm.jsx
+++ client/src/components/OrderConfirm.jsx
@@ -8,9 +8,43 @@ const OrderConfirm = () => {
 
   return (
     <>
-      <center>
-        <h1>Welcome to the Order Confirmation Page.</h1>
-      </center>
+      <div className="container my-3">
+        <h1 className="text-center">Your order has been confirm,</h1>
+        <h3 className="text-center">It will delivered soon</h3>
+      </div>
+
+      <div className="container">
+        <table className="table table-bordered border-primary bg-dark">
+          <thead className="bg-dark">
+            <tr>
+              <th scope="col" className="bg-dark text-light text-center">
+                OrderItems
+              </th>
+
+              <th scope="col" className="bg-dark text-light text-center">
+                OrderDetails & ShippingAddress
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-dark">
+            <tr>
+              <td className="bg-dark text-light">
+                <ul style={{ fontWeight: "bold" }}>
+                  <li>OrderId : </li>
+                  <li>PaymentId : </li>
+                  <li>PaymentStatus : </li>
+                  <li>Name : </li>
+                  <li>Phone : </li>
+                  <li>Country : </li>
+                  <li>State : </li>
+                  <li>PinCode : </li>
+                  <li>Near By : </li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
