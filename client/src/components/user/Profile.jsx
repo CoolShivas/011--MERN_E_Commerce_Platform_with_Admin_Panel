a2fef5f378da361ca2707d9a4a86b7b3eb192daf
CoolShivas <shivas2710cool00@gmail.com>
2025-10-08 13:36:52 +0530

    Some correction in Profile.jsx file page.

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -9,41 +9,6 @@ const Profile = () => {
       <div className="container text-center my-3">
         <h1>Welcome , {profileUser?.data?.name} </h1>
         <h3> {profileUser?.data?.email} </h3>
-        <h1>Total Order :- </h1>
-      </div>
-
-      <div className="container my-5">
-        <table className="table table-bordered border-primary bg-dark">
-          <thead className="bg-dark">
-            <tr>
-              <th scope="col" className="bg-dark text-light text-center">
-                OrderItems
-              </th>
-
-              <th scope="col" className="bg-dark text-light text-center">
-                OrderDetails & ShippingAddress
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-dark">
-            <tr>
-              <td className="bg-dark text-light">Showing Order Products</td>
-              <td className="bg-dark text-light">
-                <ul style={{ fontWeight: "bold" }}>
-                  <li>OrderId : </li>
-                  <li>PaymentId : </li>
-                  <li>PaymentStatus :</li>
-                  <li>Name : </li>
-                  <li>Phone : </li>
-                  <li>Country : </li>
-                  <li>State :</li>
-                  <li>PinCode :</li>
-                  <li>Near By : </li>
-                </ul>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </>
   );
